929cfd9793fb1597788ef3d565975eb898bf55a3
YohanPipereau <yohan.pipereau@gmail.com>
2018-06-10 14:34:22 +0200

    prevent premption and hard interrupt on the CPU during time measure

--- netlinkrw/kern/nlkern.c
+++ netlinkrw/kern/nlkern.c
@@ -2,12 +2,14 @@
  *    Description:
  *        Created:  20/05/2018
  *         Author:  Yohan Pipereau
-*/
+ */
 
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/skbuff.h>
 #include <net/netlink.h>
+#include <linux/preempt.h>
+#include <linux/hardirq.h>
 #include <linux/mutex.h>
 #include <linux/seq_file_net.h>
 
@@ -19,7 +21,7 @@ MODULE_DESCRIPTION("Multicast Hello world message !");
 #define MYMGRP 0x1
 
 struct sock *nl_sock;
-
+unsigned long flags;
 
 static void recv_message(struct sk_buff *skb)
 {
@@ -33,24 +35,24 @@ static void recv_message(struct sk_buff *skb)
 	nlhr = (struct nlmsghdr *)skb->data; //get header from received msg
 	msg_size = nlmsg_len(nlhr); //get length of received message
 
-	/* Start time measurement  */
+	/* Start time measurement; CPUID prevent out of order execution */
 	asm volatile ("CPUID\n\t"
-	"RDTSC\n\t"
-	"mov %%edx, %0\n\t"
-	"mov %%eax, %1\n\t": "=r" (cycles_high), "=r" (cycles_low)::
-	"%rax", "%rbx", "%rcx", "%rdx");
+			"RDTSC\n\t"
+			"mov %%edx, %0\n\t"
+			"mov %%eax, %1\n\t": "=r" (cycles_high),
+			"=r" (cycles_low):: "%rax", "%rbx", "%rcx", "%rdx");
 
 	/*  allocate SKBuffer freed in multicast */
 	skb_out = nlmsg_new(msg_size, 0);
 	if (!skb_out) {
-	        printk(KERN_ERR "SKB mem alloc failed\n");
+		printk(KERN_ERR "SKB mem alloc failed\n");
 	}
 
 	/* Link nlha to sbb_out */
 	nlha = nlmsg_put(skb_out, nlhr->nlmsg_pid, nlhr->nlmsg_seq,
 			NLMSG_DONE, msg_size, 0);
 	if (!nlha) {
-	        printk(KERN_ERR "SKB mem insufficient for header\n");
+		printk(KERN_ERR "SKB mem insufficient for header\n");
 	}
 
 	/*  copy head of message payload of receive message to message to send */
@@ -63,10 +65,10 @@ static void recv_message(struct sk_buff *skb)
 
 	/* End time measurement */
 	asm volatile("RDTSCP\n\t"
-	"mov %%edx, %0\n\t"
-	"mov %%eax, %1\n\t"
-	"CPUID\n\t": "=r" (cycles_high1), "=r" (cycles_low1)::
-	"%rax", "%rbx", "%rcx", "%rdx");
+			"mov %%edx, %0\n\t"
+			"mov %%eax, %1\n\t"
+			"CPUID\n\t": "=r" (cycles_high1), "=r" (cycles_low1)::
+			"%rax", "%rbx", "%rcx", "%rdx");
 
 	start = ( ((uint64_t)cycles_high << 32) | cycles_low );
 	end = ( ((uint64_t)cycles_high1 << 32) | cycles_low1 );
@@ -83,6 +85,9 @@ static int __init initfn(void)
 
 	printk(KERN_INFO "Register nlkern \n");
 
+	preempt_disable(); /*disable preemption on our CPU*/
+	raw_local_irq_save(flags); /*disable hard interrupts on our CPU*/
+
 	nl_sock = netlink_kernel_create(&init_net, NETLINK_USERSOCK, &cfg);
 	if (!nl_sock) {
 		printk(KERN_ALERT "Error creating socket\n");
@@ -97,6 +102,9 @@ static void __exit exitfn(void)
 	printk(KERN_INFO "Quit nlkern\n");
 
 	netlink_kernel_release(nl_sock);
+
+	raw_local_irq_restore(flags); /*enable hard interrupts on our CPU*/
+	preempt_enable();/*we enable preemption*/
 }
 
 module_init(initfn);
