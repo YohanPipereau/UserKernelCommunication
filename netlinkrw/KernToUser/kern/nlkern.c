ccd869d28beac7f0af3dff3528e4eca7213a5a14
YohanPipereau <yohan.pipereau@gmail.com>
2018-06-14 19:03:43 +0200

    Create a thread to send messages

--- netlinkrw/KernToUser/kern/nlkern.c
+++ netlinkrw/KernToUser/kern/nlkern.c
@@ -1,5 +1,5 @@
 /*
- *    Description:
+ *    Description: Please register this module after launching nlreaders.
  *        Created:  20/05/2018
  *         Author:  Yohan Pipereau
  */
@@ -10,6 +10,7 @@
 #include <net/netlink.h>
 #include <linux/preempt.h>
 #include <linux/hardirq.h>
+#include <linux/kthread.h>
 #include <linux/mutex.h>
 #include <linux/seq_file_net.h>
 
@@ -19,21 +20,24 @@ MODULE_DESCRIPTION("Netlink : Send message from kernel land to user land.");
 
 /* Multicast group */
 #define MYMGRP 0x1
+#define MSG_SIZE 1024
+#define NB_MSG 1024
 
+struct task_struct *task;
 struct sock *nl_sock;
 unsigned long flags;
 
-static void recv_message(struct sk_buff *skb)
+/*
+ * this is a thread function which will die eventually
+ */
+static int send_messages(void *data)
 {
 	struct sk_buff *skb_out; //SKB data to send
-	struct nlmsghdr *nlhr; //netlink header for received message
-	int msg_size;
 	struct nlmsghdr *nlha; //netlink header for answer
 	unsigned cycles_low, cycles_high, cycles_low1, cycles_high1;
 	uint64_t start, end;
-
-	nlhr = (struct nlmsghdr *)skb->data; //get header from received msg
-	msg_size = nlmsg_len(nlhr); //get length of received message
+	static int seq = 0;
+	int i;
 
 	/* Start time measurement; CPUID prevent out of order execution */
 	asm volatile ("CPUID\n\t"
@@ -42,26 +46,27 @@ static void recv_message(struct sk_buff *skb)
 			"mov %%eax, %1\n\t": "=r" (cycles_high),
 			"=r" (cycles_low):: "%rax", "%rbx", "%rcx", "%rdx");
 
-	/*  allocate SKBuffer freed in multicast */
-	skb_out = nlmsg_new(msg_size, 0);
-	if (!skb_out) {
-		printk(KERN_ERR "SKB mem alloc failed\n");
+	for (i = 0; i < NB_MSG; i++) {
+		/*  allocate SKBuffer freed in multicast */
+		skb_out = nlmsg_new(MSG_SIZE, 0);
+		if (!skb_out) {
+			printk(KERN_ERR "SKB mem alloc failed\n");
+		}
+
+		/* Link nlha to sbb_out */
+		nlha = nlmsg_put(skb_out, 0, seq,
+				NLMSG_DONE, MSG_SIZE, 0);
+		if (!nlha) {
+			printk(KERN_ERR "SKB mem insufficient for header\n");
+		}
+
+		memcpy(nlmsg_data(nlha), "A", MSG_SIZE);
+		nlmsg_end(skb_out, nlha); //end construction of nl message
+
+		/* free skb_out meanwhile */
+		nlmsg_multicast(nl_sock, skb_out, 0, MYMGRP, GFP_KERNEL);
+		seq ++;
 	}
-
-	/* Link nlha to sbb_out */
-	nlha = nlmsg_put(skb_out, nlhr->nlmsg_pid, nlhr->nlmsg_seq,
-			NLMSG_DONE, msg_size, 0);
-	if (!nlha) {
-		printk(KERN_ERR "SKB mem insufficient for header\n");
-	}
-
-	/*  copy head of message payload of receive message to message to send */
-	strncpy(nlmsg_data(nlha), nlmsg_data(nlhr), msg_size);
-
-	nlmsg_end(skb_out, nlha); //end construction of nl message
-
-	/* free skb_out meanwhile */
-	nlmsg_multicast(nl_sock, skb_out, 0, MYMGRP, GFP_KERNEL);
 
 	/* End time measurement */
 	asm volatile("RDTSCP\n\t"
@@ -73,13 +78,14 @@ static void recv_message(struct sk_buff *skb)
 	start = ( ((uint64_t)cycles_high << 32) | cycles_low );
 	end = ( ((uint64_t)cycles_high1 << 32) | cycles_low1 );
 	printk(KERN_INFO "%llu clock cycles\n", (end-start));
+
+	do_exit(0);
 }
 
 static int __init initfn(void)
 {
 	struct netlink_kernel_cfg cfg = {
 		.groups = MYMGRP,
-		.input = recv_message,
 		.flags = NL_CFG_F_NONROOT_SEND,
 	};
 
@@ -88,13 +94,20 @@ static int __init initfn(void)
 	preempt_disable(); /*disable preemption on our CPU*/
 	raw_local_irq_save(flags); /*disable hard interrupts on our CPU*/
 
+	/*  create netlink socket */
 	nl_sock = netlink_kernel_create(&init_net, NETLINK_USERSOCK, &cfg);
 	if (!nl_sock) {
 		printk(KERN_ALERT "Error creating socket\n");
 		return -10;
 	}
 
-	return 0;
+	/*  create new kthread */
+	task = kthread_create(send_messages, NULL, "netlink-thread");
+        if (IS_ERR(task))
+                return PTR_ERR(task);
+
+        wake_up_process(task);
+        return 0;
 }
 
 static void __exit exitfn(void)
