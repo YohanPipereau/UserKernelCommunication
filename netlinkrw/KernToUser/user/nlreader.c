99d03eb47c541a25e48358413eff1aff4ec725b5
YohanPipereau <yohan.pipereau@gmail.com>
2018-06-14 19:04:00 +0200

    count clock cycles during receiving

--- netlinkrw/KernToUser/user/nlreader.c
+++ netlinkrw/KernToUser/user/nlreader.c
@@ -5,10 +5,23 @@
 #include <string.h>
 #include <stdio.h>
 #include <errno.h>
+#include <stdint.h>
 
 #define MAX_PAYLOAD 1024 /* maximum payload size*/
 #define MYMGRP 21
+#define NB_MSG 1024
 
+#define start_timer() asm volatile ("CPUID\n\t" \
+"RDTSC\n\t" \
+"mov %%edx, %0\n\t" \
+"mov %%eax, %1\n\t": "=r" (cycles_high), \
+"=r" (cycles_low):: "%rax", "%rbx", "%rcx", "%rdx");
+
+#define stop_timer() asm volatile("RDTSCP\n\t" \
+"mov %%edx, %0\n\t" \
+"mov %%eax, %1\n\t" \
+"CPUID\n\t": "=r" (cycles_high1), "=r" (cycles_low1):: \
+"%rax", "%rbx", "%rcx", "%rdx");
 
 int main()
 {
@@ -20,6 +33,8 @@ int main()
 	int group = MYMGRP;
 	int res;
 	int a = 212992;
+	unsigned cycles_low, cycles_high, cycles_low1, cycles_high1;
+	uint64_t start, end;
 
 	sock_fd = socket(PF_NETLINK, SOCK_RAW, NETLINK_USERSOCK);
 	if(sock_fd<0) {
@@ -39,9 +54,9 @@ int main()
 
 	if (setsockopt(sock_fd, 270, NETLINK_ADD_MEMBERSHIP, &group,
 				sizeof(group)) < 0) {
-        	perror("setsockopt < 0\n");
-        	return -1;
-    	}
+		perror("setsockopt < 0\n");
+		return -1;
+	}
 
 	if (setsockopt(sock_fd, SOL_SOCKET, SO_RCVBUF, &a, sizeof(a)) == -1) {
 		perror("increase buffer failed");
@@ -56,17 +71,25 @@ int main()
 	msg.msg_iov = &iov;
 	msg.msg_iovlen = 1; //1 message in iov
 
-	while(1) {
-		printf("Waiting for message from kernel\n");
+	printf("Waiting for message from kernel\n");
+
+	while(nlh->nlmsg_seq < NB_MSG - 1) {
 		/* Read message from kernel */
 		res = recvmsg(sock_fd, &msg, 0);
+		if (nlh->nlmsg_seq == 0)
+			start_timer();
 		if (res == -1) {
-			perror("recv failed");
-			if (errno != ENOBUFS)
-				return errno;
+			fprintf(stderr, "%d recv failed\n", nlh->nlmsg_seq);
 		}
-		printf("[pid=%d, seq=%d] %s %d\n", nlh->nlmsg_pid,
-				nlh->nlmsg_seq, (char *)NLMSG_DATA(nlh), res);
+		/* printf("[pid=%d, seq=%d] %s %d\n", nlh->nlmsg_pid,
+		   nlh->nlmsg_seq, (char *)NLMSG_DATA(nlh), res); */
 	}
+	stop_timer();
+
+	start = ( ((uint64_t)cycles_high << 32) | cycles_low );
+	end = ( ((uint64_t)cycles_high1 << 32) | cycles_low1 );
+	printf("%lu clock cycles\n", (end-start));
+
+	printf("Finish with sequence number %d\n", nlh->nlmsg_seq);
 	close(sock_fd);
 }
