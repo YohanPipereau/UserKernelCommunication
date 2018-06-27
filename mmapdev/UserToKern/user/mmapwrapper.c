82c49cbdd1e173bf507970e4d6df105df95c3ead
YohanPipereau <yohan.pipereau@gmail.com>
2018-06-27 10:13:42 +0200

    add mmap wrapper for mmap char dev

---
+++ mmapdev/UserToKern/user/mmapwrapper.c
@@ -0,0 +1,98 @@
+/*
+ *    Description: mmap wrapper for shared memory char dev
+ *        Created:  26/06/2018
+ *         Author:  Yohan Pipereau
+ */
+
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdint.h>
+
+#define FILENAME "/dev/mmdev"
+#define MAP_LEN 1024
+#define MSG_SIZE 1024
+
+/*  Start time measurement; CPUID prevent out of order execution */
+#define start_timer() asm volatile ("CPUID\n\t" \
+		"RDTSC\n\t" \
+		"mov %%edx, %0\n\t" \
+		"mov %%eax, %1\n\t": "=r" (cycles_high), \
+		"=r" (cycles_low):: "%rax", "%rbx", "%rcx", "%rdx");
+
+
+#define stop_timer() asm volatile("RDTSCP\n\t" \
+		"mov %%edx, %0\n\t" \
+		"mov %%eax, %1\n\t" \
+		"CPUID\n\t": "=r" (cycles_high1), "=r" (cycles_low1):: \
+		"%rax", "%rbx", "%rcx", "%rdx");
+
+int main ( int argc, char *argv[] )
+{
+	int fd;
+	char *map;
+	char buffer[MSG_SIZE];
+	unsigned cycles_low, cycles_high, cycles_low1, cycles_high1;
+	uint64_t start, end;
+	int seq = 0;
+
+	fd = open(FILENAME, O_RDWR);
+	if (fd == -1) {
+		perror("failed opening " FILENAME);
+		return errno;
+	}
+
+	fgets((char*) buffer, MSG_SIZE, stdin);
+	start_timer();
+	map = (char*) mmap(NULL, MAP_LEN, PROT_READ | PROT_WRITE, MAP_SHARED,
+			fd, 0);
+	if (map == MAP_FAILED) {
+		close(fd);
+		perror("mmap failed");
+		return errno;
+	}
+
+	if (!ftruncate(fd, MAP_LEN)) {
+		perror("ftruncate failed");
+		return errno;
+	}
+
+	strcpy(map, buffer);
+	seq++;
+	stop_timer();
+	start = ( ((uint64_t)cycles_high << 32) | cycles_low );
+	end = ( ((uint64_t)cycles_high1 << 32) | cycles_low1 );
+	printf("First sending : %lu clock cycles\n", end-start);
+
+	while (1) {
+		fgets((char*) buffer, MSG_SIZE, stdin);
+		start_timer();
+		strcpy(map, buffer);
+		seq++;
+		stop_timer();
+		start = ( ((uint64_t)cycles_high << 32) | cycles_low );
+		end = ( ((uint64_t)cycles_high1 << 32) | cycles_low1 );
+		printf("Sending n° %d, %lu clock cycles\n", seq, end-start);
+	}
+
+	/*
+	 * msync not required :
+	 * https://www.spinics.net/lists/linux-c-programming/msg01027.html
+	 */
+
+	if (munmap(map, MAP_LEN) == -1) {
+		close(fd);
+		perror("failed munmap");
+		return errno;
+	}
+
+	close(fd);
+
+	return EXIT_SUCCESS;
+}
