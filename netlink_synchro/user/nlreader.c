e520d8488d2cfa09b2c33daa1e980b1b3f354329
YohanPipereau <yohan.pipereau@gmail.com>
2018-07-27 20:26:55 +0200

    clean up the code

--- netlink_synchro/user/nlreader.c
+++ netlink_synchro/user/nlreader.c
@@ -14,6 +14,10 @@
 #define MYMGRP 21
 #define NB_MSG 100000
 
+#ifndef SOL_NETLINK
+	#define SOL_NETLINK 270
+#endif
+
 #define start_timer() asm volatile ("CPUID\n\t" \
 "RDTSC\n\t" \
 "mov %%edx, %0\n\t" \
@@ -36,73 +40,84 @@ struct priv_msg {
 /** Create socket with binding and set multicast opt. */
 static int conn_init()
 {
-	int sock_fd;
 	struct sockaddr_nl src_addr, dest_addr;
+	int sock_size = 212992;
 	int group = MYMGRP;
-	int a = 212992;
 	int enobuf = 1;
+	int sock_fd;
+	int rc;
 
 	sock_fd = socket(AF_NETLINK, SOCK_RAW, NETLINK_USERSOCK);
-	if(sock_fd<0) {
-		fprintf(stderr,"socket creation failed\n");
-		exit(EXIT_FAILURE);
-	}
+	if(sock_fd < 0)
+		goto out_fail;
 
 	memset(&src_addr, 0, sizeof(src_addr));
 	src_addr.nl_family = AF_NETLINK;
 	src_addr.nl_groups = MYMGRP;
 	src_addr.nl_pid = getpid(); /* self pid */
-	if (bind(sock_fd, (struct sockaddr*)&src_addr, sizeof(src_addr)) < 0) {
-		fprintf(stderr,"binding failed");
-		exit(EXIT_FAILURE);
-	}
 
-	if (setsockopt(sock_fd, 270, NETLINK_ADD_MEMBERSHIP, &group,
-				sizeof(group)) < 0) {
-		fprintf(stderr,"setsockopt < 0\n");
-		exit(EXIT_FAILURE);
-	}
+	rc = bind(sock_fd, (struct sockaddr*)&src_addr, sizeof(src_addr));
+	if (rc < 0)
+		goto out_fail;
 
-	if (setsockopt(sock_fd, SOL_SOCKET, SO_RCVBUF, &a, sizeof(a)) == -1) {
-		fprintf(stderr,"increase buffer failed");
-		exit(EXIT_FAILURE);
-	}
+	rc = setsockopt(sock_fd, 270, NETLINK_ADD_MEMBERSHIP, &group,
+			sizeof(group));
+	if(rc < 0)
+		goto out_fail;
 
-	/* Don't ignore NOBUFS errors as return code of kernel sending */
-	if (setsockopt(sock_fd, SOL_NETLINK, NETLINK_BROADCAST_ERROR, &enobuf,
-				sizeof(enobuf)) == -1) {
-		fprintf(stderr,"ENOBUFS option activation failed");
-		exit(EXIT_FAILURE);
-	}
+	rc = setsockopt(sock_fd, SOL_SOCKET, SO_RCVBUF, &sock_size,
+			sizeof(sock_size));
+	if (rc < 0)
+		goto out_fail;
+
+	/* Do not ignore ENOBUF error after kernel send */
+	rc = setsockopt(sock_fd, SOL_NETLINK, NETLINK_BROADCAST_ERROR, &enobuf,
+			sizeof(enobuf));
+	if (rc < 0)
+		goto out_fail;
 
 	memset(&dest_addr, 0, sizeof(dest_addr));
 	dest_addr.nl_family = AF_NETLINK;
 	dest_addr.nl_pid = 0; /* For Linux Kernel */
 	/*  Prevent remote address checking after every reception */
-	if (connect(sock_fd,(struct sockaddr *) &dest_addr,
-				sizeof(dest_addr)) == -1) {
-		fprintf(stderr, "connect failed\n");
-		exit(EXIT_FAILURE);
-	}
+	rc = connect(sock_fd,(struct sockaddr *) &dest_addr, sizeof(dest_addr));
+	if (rc < 0)
+		goto out_fail;
 
 	return sock_fd;
+
+out_fail:
+	fprintf(stderr,"Init socket failed");
+	return errno;
 }
 
 static struct priv_msg* prepare_error_msg()
 {
-	struct msghdr msgerr = {0};
-	struct iovec *ioverr;
+	struct priv_msg *priv_err = NULL;
 	struct nlmsghdr *nlherr = NULL;
-	struct nlmsgerr *err;
-	struct priv_msg  *priv_err;
+	struct iovec *ioverr = NULL;
+	struct nlmsgerr *err = NULL;
+	struct msghdr msgerr = {0};
 
 	priv_err = malloc(sizeof(struct priv_msg));
+	if (!priv_err)
+		goto free_mem;
+
 	nlherr = malloc(NLMSG_SPACE(MAX_PAYLOAD));
+	if (!nlherr)
+		goto free_mem;
+
 	ioverr = malloc(sizeof(struct iovec));
+	if (!ioverr)
+		goto free_mem;
+
 	err = malloc(sizeof(struct nlmsgerr));
+	if (!err)
+		goto free_mem;
+
 	memset(nlherr, 0, NLMSG_SPACE(MAX_PAYLOAD));
 	nlherr->nlmsg_type = NLMSG_ERROR;
-	ioverr->iov_base = (void *)nlherr;
+	ioverr->iov_base = nlherr;
 	ioverr->iov_len = NLMSG_LENGTH(MAX_PAYLOAD);
 	msgerr.msg_iov = ioverr;
 	msgerr.msg_iovlen = 1; //1 message in iov
@@ -114,40 +129,74 @@ static struct priv_msg* prepare_error_msg()
 	priv_err->iov = ioverr;
 
 	return priv_err;
+
+free_mem:
+	free(priv_err);
+	free(nlherr);
+	free(ioverr);
+	free(err);
+	exit(EXIT_FAILURE);
 }
 
 static struct priv_msg* prepare_sync_msg()
 {
-	struct msghdr msg = {0};
-	struct iovec *iov;
+	struct priv_msg *priv_sync = NULL;
 	struct nlmsghdr *nlh = NULL;
-	struct priv_msg *priv_sync;
+	struct iovec *iov = NULL;
+	struct msghdr msg = {0};
 
 	priv_sync = malloc(sizeof(struct priv_msg));
+	if (!priv_sync)
+		goto free_mem;
+
 	nlh = malloc(NLMSG_SPACE(MAX_PAYLOAD));
+	if (!nlh)
+		goto free_mem;
+
 	iov = malloc(sizeof(struct iovec));
+	if (!iov)
+		goto free_mem;
+
 	memset(nlh, 0, NLMSG_SPACE(MAX_PAYLOAD));
 	iov->iov_base = (void *)nlh;
 	iov->iov_len = NLMSG_LENGTH(MAX_PAYLOAD);
 	msg.msg_iov = iov;
 	msg.msg_iovlen = 1; //1 message in iov
 	nlh->nlmsg_flags = NLM_F_DUMP | NLM_F_REQUEST;
+
 	priv_sync->msg = msg;
 	priv_sync->nlh = nlh;
 	priv_sync->err = NULL;
 	priv_sync->iov = iov;
+
+	return priv_sync;
+
+free_mem:
+	free(priv_sync);
+	free(nlh);
+	free(iov);
+	exit(EXIT_FAILURE);
 }
 
 static struct priv_msg* prepare_main_msg()
 {
+	struct priv_msg *priv_main = NULL;
+	struct nlmsghdr *nlh = NULL;
 	struct msghdr msg = {0};
 	struct iovec *iov;
-	struct nlmsghdr *nlh = NULL;
-	struct priv_msg *priv_main;
 
 	priv_main = malloc(sizeof(struct priv_msg));
+	if (!priv_main)
+		goto free_mem;
+
 	nlh = malloc(NLMSG_SPACE(MAX_PAYLOAD));
+	if (!priv_main)
+		goto free_mem;
+
 	iov = malloc(sizeof(struct iovec));
+	if (!iov)
+		goto free_mem;
+
 	memset(nlh, 0, NLMSG_SPACE(MAX_PAYLOAD));
 	iov->iov_base = (void *)nlh;
 	iov->iov_len = NLMSG_LENGTH(MAX_PAYLOAD);
@@ -160,6 +209,11 @@ static struct priv_msg* prepare_main_msg()
 	priv_main->iov = iov;
 
 	return priv_main;
+
+free_mem:
+	free(priv_main);
+	free(nlh);
+	free(iov);
 }
 
 /* Release priv_msg ressources */
@@ -177,55 +231,57 @@ void release_priv_msg(struct priv_msg *priv)
 	return;
 }
 
-void send_err_msg(int fd, struct priv_msg *priv_main, struct priv_msg *priv_err)
+static int send_err_msg(int fd, struct priv_msg *priv_main, struct priv_msg *priv_err)
 {
 	fprintf(stderr, "[seq=%d] send error msg to kernel\n",
-			priv_main->nlh->nlmsg_seq);
-	priv_err->nlh->nlmsg_seq;
+		priv_main->nlh->nlmsg_seq);
+
+	//priv_err->nlh->nlmsg_seq;
 	priv_err->err->msg = *priv_main->nlh;
 	memcpy(NLMSG_DATA(priv_err->nlh), priv_err->err,
-			sizeof(struct nlmsgerr));
-	sendmsg(fd, &priv_err->msg, 0);
+	       sizeof(struct nlmsgerr));
+
+	return sendmsg(fd, &priv_err->msg, 0);
 }
 
-void send_sync_msg(int fd, struct priv_msg *priv_sync)
+static int send_sync_msg(int fd, struct priv_msg *priv_sync)
 {
 	fprintf(stderr, "send sync message\n");
-	sendmsg(fd, &priv_sync->msg, 0);
+
+	return sendmsg(fd, &priv_sync->msg, 0);
 }
 
 static void netlink_bsd_receive(int sock_fd) {
-	struct priv_msg *priv_err, *priv_main, *priv_sync;
-	int res;
-	int *data;
 	unsigned cycles_low, cycles_high, cycles_low1, cycles_high1;
+	struct priv_msg *priv_err, *priv_main, *priv_sync;
 	uint64_t start, end;
+	int *data;
+	int rc;
 
 	priv_main = prepare_main_msg();
-	priv_err = prepare_error_msg();
 	priv_sync = prepare_sync_msg();
 
 	/* Receive from kernel */
-
 	printf("Waiting for message from kernel\n");
+
 	start_timer();
 	while(priv_main->nlh->nlmsg_seq < NB_MSG - 1) {
-		res = recvmsg(sock_fd, &priv_main->msg, MSG_ERRQUEUE);
-		if (res == -1 && errno == ENOBUFS) {
-			printf("[seq=%d] ENOBUF & res = -1\n",
-					priv_main->nlh->nlmsg_seq);
+		rc = recvmsg(sock_fd, &priv_main->msg, MSG_ERRQUEUE);
+		if (rc < 0 && errno == ENOBUFS) {
+			printf("[seq=%d] ENOBUF & rc = -1\n",
+			       priv_main->nlh->nlmsg_seq);
 			send_sync_msg(sock_fd, priv_sync);
-			res = 0;
-			errno = 0;
-		} else if (res == -1) {
+			rc = 0;
+			//errno = 0;
+		} else if (rc == -1) {
 			printf("[seq=%d] ENOBUF & errno=%d\n",
-					priv_main->nlh->nlmsg_seq, errno);
-		} else if (res > 0) {
+			       priv_main->nlh->nlmsg_seq, errno);
+		} else if (rc > 0) {
 			//message has been received
 			data = NLMSG_DATA(priv_main->nlh);
 			printf("[pid=%d, seq=%d] %d\n",
-				priv_main->nlh->nlmsg_pid,
-				priv_main->nlh->nlmsg_seq, *data);
+			       priv_main->nlh->nlmsg_pid,
+			       priv_main->nlh->nlmsg_seq, *data);
 		}
 	}
 	stop_timer();
